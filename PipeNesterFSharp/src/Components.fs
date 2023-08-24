c81a1e9ffb6f9e2cfe6f2cbd461e29554036a714
laurenpaton <laurenevepaton@gmail.com>
2023-08-24 17:00:24 +0200

    almost finished form

--- PipeNesterFSharp/src/Components.fs
+++ PipeNesterFSharp/src/Components.fs
@@ -1,5 +1,6 @@
 namespace App
 
+open Browser.Types
 open Fable.Core
 open Thoth.Fetch
 open Thoth.Json
@@ -10,7 +11,6 @@ open Fable.FontAwesome
 
 type Order =
         {
-          id: int
           orderNumber: int
           order: string
         }
@@ -18,16 +18,13 @@ type Order =
         static member Decoder =
             Decode.object (fun get ->
                   {
-                    id = get.Required.Field "id" Decode.int
-                    orderNumber =  get.Required.Field "orderNumber" Decode.int
+                    orderNumber = get.Required.Field "orderNumber" Decode.int
                     order = get.Required.Field "order" Decode.string
                   }
              )
 
-
         static member Encoder (order : Order) =
             Encode.object [
-                "id", Encode.int order.id
                 "orderNumber", Encode.int order.orderNumber
                 "order", Encode.string order.order
             ]
@@ -87,27 +84,47 @@ type Components =
 
     [<ReactComponent>]
     static member OrderForm () =
-        let (orderItems, setOrderItems) = React.useState([])
-        let (addingItem, setAddingItem) = React.useState(false)
-        let (editingItem, setEditingItem) = React.useState(false)
 
-        let mutable itemDiameterRef = React.useRef("")
-        let mutable itemQuantityRef = React.useRef("")
-        let mutable orderNumberRef = React.useRef(None)
-        let mutable orderRef = React.useRef([])
+        let (orderItems, setOrderItems) = React.useState []
+        let (addingItem, setAddingItem) = React.useState false
+        let (editingItem, setEditingItem) = React.useState false
+
+        let mutable itemDiameterRef = React.useRef None
+        let mutable itemQuantityRef = React.useRef None
+        let mutable orderNumberRef = React.useRef None
+        let mutable orderRef = React.useRef List.empty
 
         let addNewItemHandler () =
-            if (itemDiameterRef.current = "") || (itemQuantityRef.current = "") then
+            if (itemDiameterRef.current = None) || (itemQuantityRef.current = None) then
                 ()
             else
                 let newItem = [itemDiameterRef.current, itemQuantityRef.current]
+                              |> List.map unbox<HTMLInputElement>
+                              |> List.map (fun i -> i.value)
+
                 let newItems = newItem@orderItems
 
-                orderRef.current <-  newItems
-                itemDiameterRef.current <- ""
-                itemQuantityRef.current <- ""
+                orderRef.current <- newItems
+                itemDiameterRef.current <- None
+                itemQuantityRef.current <- None
                 setOrderItems newItems
                 setAddingItem false
+
+        let submitHandler () =
+            let newOrderNumber = unbox<HTMLInputElement> orderNumberRef.current
+            let newOrder = {
+                orderNumber = newOrderNumber.value |> int
+                order = orderItems |> String.concat "; "
+            }
+
+            promise {
+                let url = sprintf "https://pipenesting-default-rtdb.europe-west1.firebasedatabase.app/pipes.json"
+                return! Fetch.post (url, newOrder, caseStrategy = CamelCase)
+            }
+
+
+        let orderString =
+            orderItems |> List.fold (fun acc i -> $"({i}) {acc}") ""
 
         Html.div [
             prop.className "flex justify-items-center"
@@ -117,7 +134,7 @@ type Components =
                     prop.className "p-10 mx-10 flex w-full justify-items-center align-center"
 
                     prop.children [
-                       Html.p [
+                       Html.div [
                            prop.className "w-full"
                            (*prop.style [
                                style.borderColor color.red
@@ -128,7 +145,7 @@ type Components =
                                    prop.className "mx-5 mb-5 w-full justify-items-center align-center"
                                    prop.children [
                                        Html.label [
-                                            prop.className "text-left text-2xl p-5"
+                                            prop.className "text-left text-2xl p-5 font-semibold"
                                             prop.text "Order Number:"
                                        ]
                                    ]
@@ -139,61 +156,121 @@ type Components =
                                        Html.input [
                                           prop.id "orderNumber"
                                           prop.name "orderNumber"
-                                          prop.defaultValue ""
                                           prop.type'.number
+                                          prop.defaultValue ""
                                           prop.style [
                                               style.borderRadius 10
                                               style.borderColor color.gray
                                               style.borderWidth 1
                                           ]
-                                          prop.className "ml-10 w-96"
+                                          prop.className "ml-10 w-64 p-1"
                                           prop.ref orderNumberRef
                                       ]
                                    ]
                                ]
                                Html.div [
-                                   prop.className "p-5 w-full"
+                                   prop.className "mt-10 mx-5 w-full"
                                    prop.children [
                                        Html.label [
-                                            prop.className "p-5 text-2xl"
+                                            prop.className "text-left text-2xl p-5 font-semibold"
                                             prop.text $"Order Details:"
-                                        ]
-                                   ]
-                               ]
-                               Html.div [
-                                   prop.className ""
-                                   prop.children [
-
-                                       Html.label [
-                                           prop.className "ml-10"
-                                           prop.text $"Current Order: {orderRef.current}"
                                        ]
                                    ]
                                ]
                                Html.div [
+                                   prop.className "mt-5"
+                                   prop.children [
+                                       Html.div [
+                                           prop.children [
+                                               Html.label [
+                                                   prop.className "ml-20 mt-5 text-xl"
+                                                   prop.text $"Current Order: {orderString}"
+                                               ]
+                                           ]
+                                       ]
+                                       Html.div [
+                                           prop.className "mt-7 ml-10"
+                                           prop.children [
+                                               if (addingItem) then
+                                                   Html.div [
+                                                       prop.className "ml-5 mb-3"
+                                                       prop.children [
+                                                           Html.label [
+                                                              prop.className "p-5 ml-1 text-large"
+                                                              prop.text $"Diameter:"
+                                                            ]
+                                                           Html.input [
+                                                              prop.id "diameter"
+                                                              prop.name "diameter"
+                                                              prop.defaultValue ""
+                                                              prop.type'.number
+                                                              prop.style [
+                                                                  style.borderRadius 10
+                                                                  style.borderColor color.gray
+                                                                  style.borderWidth 1
+                                                              ]
+                                                              prop.className "ml-10 p-1"
+                                                              prop.ref itemDiameterRef
+                                                           ]
+                                                       ]
+                                                   ]
+
+                                                   Html.div [
+                                                       prop.className "ml-5 p-2 mb-10 "
+                                                       prop.children [
+                                                          Html.label [
+                                                              prop.className "p-5 text-large"
+                                                              prop.text $"Quantity:"
+                                                          ]
+                                                          Html.input [
+                                                            prop.id "quantity"
+                                                            prop.name "quantity"
+                                                            prop.defaultValue ""
+                                                            prop.type'.number
+                                                            prop.style [
+                                                                style.borderRadius 10
+                                                                style.borderColor color.gray
+                                                                style.borderWidth 1
+                                                            ]
+                                                            prop.className "ml-10 p-1"
+                                                            prop.ref itemQuantityRef
+                                                         ]
+                                                       ]
+                                                   ]
+
+
+                                               ]
+                                           ]
+
+                                       ]
+
+                                    ]
+                               Html.div [
                                    prop.className "flex justify-end w-3/4 mr-20"
                                    prop.children [
-                                       if (not editingItem) then
+                                       if (not addingItem) then
                                            Html.button [
                                                prop.className "cursor-pointer bg-teal-500 rounded p-3 text-large text-black hover:bg-teal-600"
                                                prop.text "Add Item to Order"
+                                               prop.onClick (fun _ -> setAddingItem true)
                                            ]
                                    ]
                                ]
                                Html.div [
                                    prop.className "flex w-3/4 items-center justify-end mr-20 gap-5 mt-10 ml-10"
                                    prop.children [
-                                       if editingItem then
+                                       if addingItem then
                                            Html.button [
-                                               prop.className "cursor-pointer bg-green-500 rounded p-3 text-large text-black hover:bg-green-600"
+                                               prop.className "cursor-pointer bg-green-500 rounded p-3 px-4 text-large text-black hover:bg-green-600"
                                                prop.text "Add Item"
+                                               prop.onClick (fun _ -> addNewItemHandler())
                                            ]
 
                                            Html.button [
-                                                Html.button [
-                                                    prop.className "cursor-pointer bg-gray-300 rounded p-3 text-large text-black hover:bg-red-300"
-                                                    prop.text "Cancel Order"
-                                                ]
+                                                    prop.className "cursor-pointer bg-gray-300 rounded p-3 px-6 text-large text-black hover:bg-red-300"
+                                                    prop.text "Close"
+                                                    prop.onClick (fun _ -> setAddingItem false)
+
                                            ]
 
                                        else
@@ -202,10 +279,9 @@ type Components =
                                                prop.text "Save Order"
                                            ]
                                            Html.button [
-                                                Html.button [
                                                     prop.className "cursor-pointer bg-gray-300 rounded p-3 text-large text-black hover:bg-red-300"
                                                     prop.text "Cancel Order"
-                                                ]
+                                                    prop.onClick (fun _ -> Router.navigate("allOrders"))
                                            ]
                                    ]
                                ]
@@ -238,34 +314,23 @@ type Components =
             |> ignore // ignore promise after state has been set as React.useEffect always needs to return a unit
         )
 
-        //let orders =
-        //        promise {
-        //            let url = "https://pipenesting-default-rtdb.europe-west1.firebasedatabase.app/pipes.json"
-        //            return! Fetch.get(url, decoder = Decode.list Order.Decoder)
-        //        }
 
-        //let orders =
-        //    [
-        //        {
-        //            id =  1
-        //            orderNumber = 1
-        //            order = "[(3,25), (2, 75), (5,110)]"
-        //        };
-        //        {
-        //            id =  2
-        //            orderNumber = 2
-        //            order = "[(5,25), (1, 75), (10,110)]"
-        //        }
-        //    ]
+        let getListItems (orderString : string) =
+            let removeSquareBrackets = orderString.Trim('[', ']').Trim()
+            let splitItems = removeSquareBrackets.Split(';')
+                             |> Array.toList
+                             //|> List.map (fun i -> i.Trim('(', ')'))
 
-        //let listItems =
-        //    orders |> Array.map
-        //                  (fun item ->
-        //                     let order = item.order
-        //                     Html.li [
-        //                         prop.text $"{order}"
-        //                     ])
+            Html.ul [
 
+                prop.children (splitItems
+                |> List.map(fun item ->
+                    Html.li [
+                        prop.text $"{item}"
+                        prop.onClick (fun _ -> Router.navigate(""))
+                    ]
+                ))
+            ]
 
         Html.div [
             prop.className "px-20"
@@ -292,18 +357,15 @@ type Components =
                                |> Array.toList
                                |> List.mapi (fun index orderItem ->
                                    Html.tr [
-                                       prop.key orderItem.id
+                                       prop.key orderItem.orderNumber
                                        prop.className (if index % 2 = 0 then "bg-gray-100" else "bg-white border-b dark:border-neutral-500")
                                        prop.children [
                                            Html.td [
                                                prop.text $"{orderItem.orderNumber}"
                                            ]
                                            Html.td [
-                                               prop.children [
-                                                   Html.ul [
-                                                       prop.text $"{orderItem.order}"
-                                                   ]
-                                               ]
+                                               prop.children (getListItems orderItem.order)
+
                                            ]
                                        ]
                                    ]
