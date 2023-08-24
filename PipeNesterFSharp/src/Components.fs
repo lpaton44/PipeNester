feaf6db989417d97d023d41ae7d369f4fb89d83d
laurenpaton <laurenevepaton@gmail.com>
2023-08-24 11:30:51 +0200

    Working on Form

--- PipeNesterFSharp/src/Components.fs
+++ PipeNesterFSharp/src/Components.fs
@@ -62,13 +62,21 @@ type Components =
                     prop.className "items-bottom justify-end flex mr-10 mb-5"
                     prop.children[
                         Html.nav [
-                            prop.className "mr-20 link text-white text-xl hover:text-gray-500"
-                            prop.text "All Orders"
+                            prop.className "mr-10 p-5 link text-white font-semibold text-xl"
+                            prop.children [
+                                Html.a [
+                                    prop.onClick (fun _ -> Router.navigate "allOrders")
+                                    prop.className "mr-10 hover:text-gray-500"
+                                    prop.text "All Orders"
+                                ]
+                                Html.a [
+                                    prop.onClick (fun _ -> Router.navigate "newOrder")
+                                    prop.className "hover:text-gray-500"
+                                    prop.text "Add Order"
+                                ]
+                            ]
                         ]
-                        Html.nav [
-                            prop.className "mr-20 link text-white text-xl hover:text-gray-500"
-                            prop.text "New Order"
-                        ]
+
                     ]
 
                 ]
@@ -79,14 +87,6 @@ type Components =
 
     [<ReactComponent>]
     static member OrderForm () =
-
-        let orders =
-                promise {
-                    let url = "https://pipenesting-default-rtdb.europe-west1.firebasedatabase.app/pipes.json"
-                    return! Fetch.get(url, decoder = Decode.list Order.Decoder)
-                }
-
-
         let (orderItems, setOrderItems) = React.useState([])
         let (addingItem, setAddingItem) = React.useState(false)
         let (editingItem, setEditingItem) = React.useState(false)
@@ -110,49 +110,75 @@ type Components =
                 setAddingItem false
 
         Html.div [
-            prop.className "items-center justify-center"
+            prop.className "flex justify-items-start "
 
             prop.children [
                 Html.form [
+                    prop.className "ml-10 w-full"
+
                     prop.children [
                        Html.p [
+                           prop.className "ml-10 w-full"
+                           (*prop.style [
+                               style.borderColor color.red
+                               style.borderWidth 1
+                           ]*)
                            prop.children [
-                               Html.label [
-                                    prop.className "mt-20 text-xl p-5 font-semibold "
-                                    prop.text "Order Number:"
-                                ]
-                               Html.input [
-                                   prop.id "orderNumber"
-                                   prop.name "orderNumber"
-                                   prop.defaultValue ""
-                                   prop.type'.text
-                                   prop.style [
-                                       style.borderRadius 10
-                                       style.borderColor color.gray
-                                       style.borderWidth 1
+                               Html.div [
+                                   prop.className "flex items-start w-full"
+                                   prop.children [
+                                       Html.label [
+                                            prop.className "text-left text-2xl p-5 font-semibold "
+                                            prop.text "Order Number:"
+                                       ]
                                    ]
-                                   prop.className "w-7/10 mb-10"
                                ]
-                               Html.label [
-                                   prop.className "mt-20 text-xl p-5 font-semibold"
-                                   prop.text $"Order Details: {orders}"
-                               ]
-                               Html.input [
-                                   prop.id "order"
-                                   prop.name "order"
-                                   prop.defaultValue ""
-                                   prop.type'.text
-                                   prop.style [
-                                       style.borderRadius 10
-                                       style.borderColor color.gray
-                                       style.borderWidth 1
+                               Html.div [
+                                   prop.className "flex items-start"
+                                   prop.children [
+                                       Html.input [
+                                          prop.id "orderNumber"
+                                          prop.name "orderNumber"
+                                          prop.defaultValue ""
+                                          prop.type'.number
+                                          prop.style [
+                                              style.borderRadius 10
+                                              style.borderColor color.gray
+                                              style.borderWidth 1
+                                          ]
+                                          prop.className "ml-3 w-7/10"
+                                          prop.ref orderNumberRef
+                                      ]
                                    ]
-                                   prop.className "w-7/10 ml-2"
                                ]
-
+                               Html.div [
+                                   prop.className "flex items-start"
+                                   prop.children [
+                                       Html.label [
+                                            prop.className " p-5 text-2xl p-5 font-semibold"
+                                            prop.text $"Order Details:"
+                                        ]
+                                   ]
+                               ]
+                               Html.div [
+                                   prop.className "flex items-start"
+                                   prop.children [
+                                       Html.input [
+                                            prop.id "order"
+                                            prop.name "order"
+                                            prop.defaultValue ""
+                                            prop.type'.text
+                                            prop.style [
+                                                style.borderRadius 10
+                                                style.borderColor color.gray
+                                                style.borderWidth 1
+                                            ]
+                                            prop.className " mb-10 w-7/10 ml-2"
+                                        ]
+                                   ]
+                               ]
                            ]
                        ]
-
                     ]
                 ]
             ]
@@ -161,11 +187,30 @@ type Components =
 
 
 
-    (*[<ReactComponent>]
-    static member OrderList() =
-        let orders = [
 
-        ]
+    [<ReactComponent>]
+    static member OrderList() =
+
+        let orders =
+                promise {
+                    let url = "https://pipenesting-default-rtdb.europe-west1.firebasedatabase.app/pipes.json"
+                    return! Fetch.get(url, decoder = Decode.list Order.Decoder)
+                }
+
+        let orders =
+            [
+                {
+                    id =  1
+                    orderNumber = 1
+                    order = "[(3,25), (2, 75), (5,110)]"
+                };
+                {
+                    id =  2
+                    orderNumber = 2
+                    order = "[(5,25), (1, 75), (10,110)]"
+                }
+            ]
+
         let listItems =
             orders |> List.map
                           (fun item ->
@@ -187,50 +232,39 @@ type Components =
                             Html.tr [
                                 Html.th [ prop.text "Order Number" ]
                                 Html.th [ prop.text "Order Summary" ]
-                            ]
-                            Html.tbody [
-                                prop.children (
-                                    orders
-                                    |> List.mapi (fun index orderItem ->
-                                        Html.tr [
-                                            prop.key orderItem.id
-                                            prop.className (if index % 2 = 0 then "bg-gray-100" else "bg-white border-b dark:border-neutral-500")
-                                            prop.children [
-
-                                            ]
-                                        ]
-
-                                    ))
+                                ]
                             ]
                         ]
+                        Html.tbody [
+                           prop.children (
+                               orders
+                               |> List.mapi (fun index orderItem ->
+                                   Html.tr [
+                                       prop.key orderItem.id
+                                       prop.className (if index % 2 = 0 then "bg-gray-100" else "bg-white border-b dark:border-neutral-500")
+                                       prop.children [
+                                           Html.td [
+
+                                               prop.text $"{orderItem.orderNumber}"
+                                           ]
+                                           Html.td [
+                                               prop.children [
+                                                   Html.ul [
+                                                       prop.text $"{orderItem.order}"
+                                                   ]
+                                               ]
+                                           ]
+                                       ]
+                                   ]
+                               )
+                           )
+                       ]
+                    ]
                 ]
             ]
-
         ]
-    ]
-    ]
-*)
-    /// <summary>
-    /// The simplest possible React component.
-    /// Shows a header with the text Hello World
-    /// </summary>
-    [<ReactComponent>]
-    static member HelloWorld() = Html.h1 "Hello World"
 
-    /// <summary>
-    /// A stateful React component that maintains a counter
-    /// </summary>
-    [<ReactComponent>]
-    static member Counter() =
-        let (count, setCount) = React.useState(0)
-        Html.div [
-            Html.h1 count
-            Html.button [
-                prop.className "bg-orange-500 rounded-full p-2"
-                prop.onClick (fun _ -> setCount(count + 1))
-                prop.text "Increment"
-            ]
-        ]
+
 
     /// <summary>
     /// A React component that uses Feliz.Router
@@ -246,6 +280,7 @@ type Components =
 
                 match currentUrl with
                 | [ "newOrder" ] -> Components.OrderForm()
+                | ["allOrders"] -> Components.OrderList()
                 | otherwise -> Html.h1 "Not found"
             ]
         ]
